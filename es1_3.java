c49fde1c7bfbeb3e793a426a028fdc3a5f05f62e
Raffaele Melluso <raffaelemelluso01@gmail.com>
2023-10-12 16:37:48 +0200

    Finito esercizio 1.3

--- es1_3.java
+++ es1_3.java
@@ -3,7 +3,7 @@ public class es1_3 {
     {
         int state=0;
         int i=0;
-        while(state >= 0 && i<s.length)
+        while(state >= 0 && i<s.length())
         {
             final char ch = s.charAt(i++);
             switch (state)
@@ -21,18 +21,18 @@ public class es1_3 {
                 break;
 
                 case 1:
-                    if(ch>='0' && ch<='9'&& (int)ch%2==0)
+                    if((ch>='0' && ch<='9')&& (ch%2==0))
                         state=2;
-                    else if((ch>='L' && ch<='Z') && (ch>='l' && ch<='z'))
+                    else if((ch>='L' && ch<='Z') || (ch>='l' && ch<='z'))
                         state=3;
                     else
                         state=-1;
                 break;
 
                 case 2:
-                if(ch>='0' && ch<='9'&& (int)ch%2!=0)
+                if(ch>='0' && ch<='9'&& ch%2!=0)
                     state=2;
-                else if((ch>='A' && ch<='K') && (ch>='a' && ch<='k'))
+                else if((ch>='A' && ch<='K') || (ch>='a' && ch<='k'))
                     state=3;
                 else
                     state=-1;
@@ -40,15 +40,15 @@ public class es1_3 {
 
                 case 3:
                     if( ch>='A' && ch<='z')
+                        state=3;
+                    else
                         state=-1;
                 break;
-            }
-
-        }
+            }        }
         return state==3;
     }
     public static void main(String [] args)
     {
-        System.out.println(scan("1045495"));7
+        System.out.println(scan("4A"));
     }
 }
