c7737524347fc41e3065eaf0db7e2f52261dc083
Raffaele Melluso <raffaelemelluso01@gmail.com>
2023-10-14 12:35:09 +0200

    inizio esercizio 1.4

---
+++ es1_4.java
@@ -0,0 +1,13 @@
+public class es1_4 {
+    public static boolean scan(String s)
+    {
+        int state=0;
+        int i=0;
+        while(state>=0 && i<s.length())
+        {
+            final char ch=s.charAt(i++);
+            
+        }
+        return true;
+    }
+}
