2784f939282f32af98683a68bf42e5183e7e2adf
Raffaele Melluso <raffaelemelluso01@gmail.com>
2023-10-14 15:59:26 +0200

    finiti esercizi 1.5 e 1.6

---
+++ es1_5.java
@@ -0,0 +1,51 @@
+public class es1_5 {
+
+    public static boolean scan(String s)
+    {
+        int state=0;
+        int i=0;
+        while(state>=0 && i<s.length())
+        {
+            final char c = s.charAt(i++);
+            switch(state)
+            {
+                case 0:
+                    if(c=='/')
+                        state=1;
+                    else
+                        state=-1;
+                break;
+                case 1:
+                    if(c=='*')
+                        state=2;
+                    else
+                        state=-1;
+                break;
+                case 2:
+                    if(c=='*')
+                        state=3;
+                    else if(c=='a' || c=='/')
+                        state=2;
+                    else
+                        state=-1;
+                break;
+                case 3:
+                    if(c=='/')
+                        state=4;
+                    else if(c=='a' || c=='*')
+                        state=3;
+                    else
+                        state=-1;
+                break;
+                case 4:
+                    state=-1;
+                break;
+            }
+        }
+        return state==4;
+    }
+    public static void main(String[] args) {
+        System.out.println(scan("/*/"));
+    }
+    
+}
