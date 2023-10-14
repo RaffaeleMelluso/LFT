e459f803cb5068bb37dd6955260a5950cfc9d4c3
Raffaele Melluso <raffaelemelluso01@gmail.com>
2023-10-14 13:29:49 +0200

    finito+debug es1.4

--- es1_4.java
+++ es1_4.java
@@ -3,11 +3,72 @@ public class es1_4 {
     {
         int state=0;
         int i=0;
+        
         while(state>=0 && i<s.length())
         {
-            final char ch=s.charAt(i++);
-            
+            final char c=s.charAt(i++);
+            switch(state)
+            {
+                case 0:
+                    if(c!='+' && c!='-' && c!='.' && (c<'0' || c>'9'))
+                        state=-1;
+                    else if(c=='.')
+                        state=3;
+                    else if(c=='+' || c=='-')
+                        state=1;
+                    else if(c>='0' && c<='9')
+                        state=2;
+                break;
+                    
+                case 1:
+                    if(c!='.' && (c<'0' || c>'9'))
+                        state=-1;
+                    else if(c=='.')
+                        state=3;
+                    else if(c>='0' && c<='9')
+                        state=2;
+                break;
+
+                case 2:
+                    if(c!='e' && c!='.' && (c<'0' || c>'9'))
+                        state=-1;
+                    else if(c=='e')
+                        state=5;
+                    else if(c=='.')
+                        state=3;
+                break;
+
+                case 3:
+                    if(c<'0' || c>'9')
+                        state=-1;
+                    else if(c>='0' && c<='9')
+                        state=4;
+                break;
+
+                case 4:
+                    if(c!='e' && (c<'0' || c>'9'))
+                        state=-1;
+                    else if(c=='e')
+                        state=5;
+                break;
+
+                case 5:
+                    if(c!='+' && c!='-' && (c<'0' || c>'9'))
+                        state=-1;
+                    else
+                        state=6;
+                break;
+
+                case 6:
+                    if(c<'0' || c>'9')
+                        state=-1;
+                break;
+            }
         }
-        return true;
+        return state==2 || state==4 || state==6;
+    }
+    public static void main(String [] args)
+    {
+        System.out.println(scan("++3"));
     }
 }
