0004dd62e2a59eb98606fa64624cc37263babe56
Raffaele Melluso <raffaelemelluso01@gmail.com>
2023-11-29 15:16:21 +0100

    Quasi finito parser

---
+++ lezione5l/Parser.java
@@ -0,0 +1,324 @@
+package lezione5l;
+
+import java.io.*;
+
+import lezione5l.Tag;
+import lezione5l.Token;
+
+public class Parser 
+{
+    private Lexer lex;
+    private BufferedReader pbr;
+    private Token look;
+
+    public Parser(Lexer l, BufferedReader br) 
+    {
+        lex = l;
+        pbr = br;
+        move();
+    }
+
+    void move() 
+    {
+        look = lex.lexical_scan(pbr);
+        System.out.println("token = " + look);
+    }
+
+    void error(String s) {
+	throw new Error("near line " + lex.line + ": " + s);
+    }
+
+    void match(int t) 
+    {
+        if (look.tag == t) 
+        {
+            if (look.tag != Tag.EOF) 
+                move();
+        } 
+        else error("syntax error");
+    }
+
+    public void prog() {
+	// ... completare ...
+    if(look.tag==Word.assign.tag || look.tag==Word.print.tag || look.tag==Word.read.tag || 
+        look.tag==Word.fortok.tag || look.tag==Word.iftok.tag || look.tag==Token.lpg.tag)
+    {
+        statlist();
+        match(Tag.EOF);
+    }
+	else
+        error("No such guide for prog");
+	// ... completare ...
+    }
+
+    private void statlist() 
+    {
+	    // ... completare ...
+        if(look.tag==Word.assign.tag || look.tag==Word.print.tag || look.tag==Word.read.tag || 
+            look.tag==Word.fortok.tag || look.tag==Word.iftok.tag || look.tag==Token.lpg.tag)
+        {
+           stat();
+           statlistp();
+        }
+        else
+            error("No such guide for statlist");
+    
+    
+    }
+    private void statlistp() 
+    {
+	    // ... completare ...
+        if(look.tag==Token.semicolon.tag)
+        {
+           match(Token.semicolon.tag);
+           stat();
+           statlistp();
+        }
+        else if(look.tag==Token.rpg.tag || look.tag==Tag.EOF)
+        {
+
+        }
+        else
+            error("No such guide for statlistp");
+    
+    
+    }
+
+    private void stat() 
+    {
+        switch (look.tag) 
+        {
+            case Word.assign.tag:
+                match(Word.assign.tag);
+                assignlist();
+                break;
+            case Word.print.tag:
+                match(Word.print.tag);
+                match(Token.lpt.tag);
+                exprlist();
+                match(Token.rpt.tag);
+                break;
+            case Word.read.tag:
+                match(Token.lpt.tag);
+                idlist();
+                match(Token.rpt.tag);
+                break;
+            
+            case Word.fortok.tag:
+                match(Word.fortok.tag);
+                match(Token.lpt.tag);
+                A();
+                break;
+            case Word.iftok.tag:
+                match(Word.iftok.tag);
+                match(Token.lpt.tag);
+                bexpr();
+                match(Token.rpt.tag);
+                stat();
+                B();
+                break;
+            case Token.lpg.tag:
+                match(Token.lpg.tag);
+                statlist();
+                match(Token.rpg.tag);
+                break;
+
+            default:
+                error("No such guide for stat");
+                break;
+            // ... completare ...
+        }
+    }
+    private void A()
+    {
+        switch (look.tag) {
+            case Tag.ID:
+                match(Tag.ID);
+                match(Word.init.tag);
+                expr();
+                match(Token.semicolon.tag);
+                bexpr();
+                match(Token.lpt.tag);
+                match(Word.dotok.tag);
+                stat();
+                break;
+            case Tag.RELOP:
+                bexpr();
+                match(Token.rpt.tag);
+                match(Word.dotok.tag);
+                stat();
+                break;
+            default:
+                error("No such guide for stat");
+                break;
+        }
+    }
+    private void B()
+    {
+        if(look.tag== Word.elsetok.tag)
+        {
+            match(Word.elsetok.tag);
+            stat();
+            match(Word.end.tag);
+        }
+        else if(look.tag==Word.end.tag){
+            match(Word.end.tag);
+        }
+        else
+            error("No such guide for stat");
+                
+            
+    }
+
+    private void assignlist() 
+    {
+        if(look.tag==Token.lpq.tag)
+        {
+            match(Token.lpq.tag);
+            expr();
+            match(Word.to.tag);
+            idlist();
+            match(Token.rpq.tag);
+            assignlistp();
+        }
+        else
+            error("No such guide for assignlist");
+        // ... completare ...
+    }
+    private void assignlistp()
+    {
+        if(look.tag==Token.lpq.tag)
+        {
+            match(Token.lpq.tag);
+            expr();
+            match(Word.to.tag);
+            idlist();
+            match(Token.rpq.tag);
+            assignlistp();
+        }
+        else if(look.tag==Word.end.tag || look.tag==Tag.EOF || look.tag==Token.rpg.tag)
+        {
+            
+        }
+        else
+            error("No such guide for assignlistp");
+    }
+    private void idlist()
+    {
+        if(look.tag==Tag.ID)
+        {
+            match(Tag.ID);
+            idlistp();
+        }
+        else
+            error("No such guide for idlist");
+
+    }
+    private void idlistp()
+    {
+        if(look.tag==Token.comma.tag)
+        {
+            match(Token.comma.tag);
+            match(Tag.ID);
+            idlistp();
+        }
+        else if(look.tag==Token.rpq.tag || look.tag==Token.rpt.tag)
+        {
+
+        }
+        else
+            error("No such guide for idlistp");
+    }
+    private void bexpr()
+    {
+        if(look.tag==Tag.RELOP)
+        {
+            match(Tag.RELOP);
+            expr();
+            expr();
+        }
+        else
+            error("No such guide for bexpr");
+    }
+    private void expr() 
+    {
+        switch (look.tag) 
+        {
+            case '+':
+                match(Token.plus.tag);
+                match(Token.lpt.tag);
+                exprlist();
+                match(Token.rpt.tag);
+
+                break;
+            case '*':
+                match(Token.mult.tag);
+                match(Token.lpt.tag);
+                exprlist();
+                match(Token.rpt.tag);
+                break;
+            case '-':
+                match(Token.minus.tag);
+                expr();
+                expr();
+                break;
+            case '/':
+                match(Token.div.tag);
+                expr();
+                expr();
+                break;
+            case Tag.NUM:
+                match(Tag.NUM);
+                break;
+            case Tag.ID:
+                match(Tag.ID);
+                break;
+            default:
+                error("No such guide for expr");
+                break;
+        }
+        // ... completare ...
+    }
+
+    private void exprlist() 
+    {
+        switch (look.tag) 
+        {
+            case '+':
+            case '*':
+            case '-':
+            case '/':
+            case Tag.NUM:
+            case Tag.ID:
+                expr();
+                exprlist();
+                break;
+            default:
+                error("No such guide for exprlist");
+                break;
+        }
+        // ... completare ...
+    }
+	private void exprlistp()
+    {
+        if(look.tag==Token.comma.tag)
+            match(Token.comma.tag);
+        else if(look.tag==Token.rpt.tag)
+        {}
+        else 
+            error("No such guide for exprlistp");
+    }
+    public static void main(String[] args) 
+    {
+        Lexer lex = new Lexer();
+        String path = "lezione5l/lexer.txt"; // il percorso del file da leggere
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            Parser parser = new Parser(lex, br);
+            parser.start();
+            System.out.println("Input OK");
+            br.close();
+        } catch (IOException e) {e.printStackTrace();}
+    }
+}
+//ciao
